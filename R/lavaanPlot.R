c1f1b9d1cc7bc9d40c6723b6a2444a6361c42158
Alex Lishinski <alexlishinski@gmail.com>
2021-02-03 15:50:03 +0000

    version 0.6.0

--- R/lavaanPlot.R
+++ R/lavaanPlot.R
@@ -6,8 +6,9 @@
 #' @param stand Should the coefficients being used be standardized coefficients
 #' @param covs Should model covariances be included in the diagram
 #' @param stars a character vector indicating which parameters should include significance stars be included for regression paths, latent paths, or covariances. Include which of the 3 you want ("regress", "latent", "covs"), default is none.
+#' @param digits A number indicating the desired number of digits for the coefficient values in the plot
 #' @importFrom stringr str_replace_all
-buildPaths <- function(fit, coefs = FALSE, sig = 1.00, stand = FALSE, covs = FALSE, stars = NULL){
+buildPaths <- function(fit, coefs = FALSE, sig = 1.00, stand = FALSE, covs = FALSE, stars = NULL, digits = 2){
   if(stand){
     ParTable <- lavaan::standardizedsolution(fit)
     ParTableAlt <- fit@ParTable
@@ -22,22 +23,24 @@ buildPaths <- function(fit, coefs = FALSE, sig = 1.00, stand = FALSE, covs = FAL
 
   regress <- ParTable$op == "~"
   latent <- ParTable$op == "=~"
-  cov <- ParTable$op == "~~" & (ParTable$rhs %in% ParTable$lhs[latent | regress]) & (ParTable$rhs != ParTable$lhs)
+  #cov <- ParTable$op == "~~" & (ParTable$rhs %in% ParTable$lhs[latent | regress]) & (ParTable$rhs != ParTable$lhs)
+  cov <- ParTable$op == "~~" & (ParTable$rhs != ParTable$lhs)
+
 
   zval_reg <- ParTableAlt$est[regress] / ParTableAlt$se[regress]
   pval_reg <- (1 - stats::pnorm(abs(zval_reg))) * 2
   signif_reg <- pval_reg < sig
-  coef <- ifelse(signif_reg, round(ParTable$est[regress], digits = 2), "")
+  coef <- ifelse(signif_reg, round(ParTable$est[regress], digits = digits), "")
 
   zval_lat <- ParTableAlt$est[latent] / ParTableAlt$se[latent]
   pval_lat <- (1 - stats::pnorm(abs(zval_lat))) * 2
   signif_lat <- pval_lat < sig
-  latent_coef <- ifelse(signif_lat, round(ParTable$est[latent], digits = 2), "")
+  latent_coef <- ifelse(signif_lat, round(ParTable$est[latent], digits = digits), "")
 
   zval_cov <- ParTableAlt$est[cov] / ParTableAlt$se[cov]
   pval_cov <- (1 - stats::pnorm(abs(zval_cov))) * 2
   signif_cov <- pval_cov < sig
-  cov_vals <- ifelse(signif_cov, round(ParTable$est[cov], digits = 2), "")
+  cov_vals <- ifelse(signif_cov, round(ParTable$est[cov], digits = digits), "")
 
   if("regress" %in% stars){
     #pval_reg <- ParTable$pvalue[regress]
@@ -83,7 +86,7 @@ buildPaths <- function(fit, coefs = FALSE, sig = 1.00, stand = FALSE, covs = FAL
     if(covs){
 
       covVals <- round(ParTable$est[cov], digits = 2)
-
+      if(coefs) {
       cov_paths <- paste(
         paste(
           ParTable$rhs[cov],
@@ -91,6 +94,16 @@ buildPaths <- function(fit, coefs = FALSE, sig = 1.00, stand = FALSE, covs = FAL
         paste("[label = '", cov_vals, stars_cov, "', dir = 'both']", sep = ""),
         collapse = " "
       )
+    } else {
+      cov_paths <- paste(
+        paste(
+          ParTable$rhs[cov],
+          ParTable$lhs[cov], sep = " -> "),
+        paste("[dir = 'both']", sep = ""),
+        collapse = " "
+      )
+    }
+
 
     } else {
       cov_paths <- ""
@@ -148,6 +161,7 @@ sig_stars <- function(pvals){
 #'
 #' @param label_list A named list of variable labels.
 buildLabels <- function(label_list){
+  names(label_list) <- stringr::str_replace_all(names(label_list), pattern = "\\.", replacement = "")
   labs <- paste(names(label_list), " [label = ", "'", label_list, "'", "]", sep = "")
   paste(labs, collapse = "\n")
 }
